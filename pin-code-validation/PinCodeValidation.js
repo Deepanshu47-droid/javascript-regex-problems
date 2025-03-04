96960fb9c5298a4d8081439adf51cadaabc9d92b
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-03-04 12:54:19 +0530

    [Deepanshu malviya]- Add. Make sure 400 088 is also valid along with 400088

--- pin-code-validation/PinCodeValidation.js
+++ pin-code-validation/PinCodeValidation.js
@@ -3,7 +3,8 @@ function validatePinCode(pinCode) {
     // UC-01 : Regex pattern to validate a 6-digit Indian PIN code
     // UC-02 : Starts with digit 1-9
     // UC-03 : No extra characters (like alphabets or special characters) at the end
-    const pinCodePattern = /^[1-9][0-9]{5}$/;
+    // UC-04 : Make sure 400 088 is also valid along with 400088
+    const pinCodePattern = /^[0-9]{3}\s?[0-9]{3}$/;
 
     if (pinCodePattern.test(pinCode)) {
         console.log("Valid PIN code");
@@ -17,3 +18,4 @@ validatePinCode("400088");
 validatePinCode("012345");  
 validatePinCode("400088@"); 
 validatePinCode("400088 "); 
+validatePinCode("400 088");
